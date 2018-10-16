c2a9b2ef337dd1914e3329787ad10e391a590cb1
andersonicoutos <23392311+andersonicoutos@users.noreply.github.com>
2018-10-16 16:22:58 -0300

    A  public/style.css M  table/post M  view/criarPost.php

--- view/criarPost.php
+++ view/criarPost.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Criar post</title>
-    </head>
-    <body>
-        <center>
+<head>
+    <meta charset="utf-8">
+    <title>Criar post</title>
+    <?php view('inc/header'); ?>
+</head>
+<body>
+    <center>
         <form class="" action="criarPost" method="post">
             <label for="">Titulo do post</label><br>
             <input type="title" name="title" size="67"><br>
+            <label for="">Descrição</label><br>
+            <input type="text" name="description" value="" size="67"><br>
             <label for="">Post</label><br>
-            <textarea name="content" rows="32" cols="80"></textarea><br>
-            <button type="submit">Salvar</button>
+            <textarea name="content" rows="25" cols="80"></textarea><br>
+            <button type="submit" name="draft" value="1">
+                Salvar como rascunho
+            </button>
+            <button type="submit" name="draft" value="0">
+                Publicar
+            </button>
         </form>
     </center>
-    </body>
+</body>
 </html>
